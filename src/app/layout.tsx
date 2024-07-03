db91d8f6354634187aac5ea185b80e4345aea564
M Query <135724390+Meqr1@users.noreply.github.com>
2024-07-03 14:40:53 +0530

    fix head again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import { NextUIProvider } from "@nextui-org/react";
 import Navbar from "./Navbar";
 import "./globals.css";
-import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MQuery, Meqr, Ayan Mahajan full stack developer portfolio website",
+  description: "MQuery, Meqr, Ayan Mahajan who is the full stack developer behind the app NuraNote's portfolio website",
+  keywords: "MQuery, Meqr, Ayan Mahajan, full stack developer, portfolio, who is Ayan Mahjahan?, who is Meqr?, who is MQuery?",
+  authors: { 'url': 'https://meqr.vercel.app', name: 'Ayan Mahajan' },
+}
 
 export default function RootLayout({
   children,
@@ -11,13 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" style={{ overflowX: "hidden" }}>
-      <Head>
-        <meta name="google-site-verification" content="na_tZta5zaB_Xxa0FQIp6p8Ul908Y2rjpRNgysQ_OBc" />
-        <title>MQuery, Meqr, Ayan Mahajan full stack developer portfolio website</title>
-        <meta name="description" content="MQuery, Meqr, Ayan Mahajan who is the full stack developer behind the app NuraNote's portfolio website" />
-        <meta name="keywords" content="MQuery, Meqr, Ayan Mahajan, full stack developer, portfolio, who is Ayan Mahjahan?, who is Meqr?, who is MQuery?" />
-        <meta name="author" content="Ayan Mahajan" />
-      </Head>
       <body style={{ overflowX: "hidden" }}>
         <NextUIProvider>
           <Navbar />
