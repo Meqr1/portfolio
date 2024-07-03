108051766d1916d013613c425e9c4239308a1544
M Query <135724390+Meqr1@users.noreply.github.com>
2024-07-03 14:28:59 +0530

    added support for search engine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Button, NextUIProvider } from "@nextui-org/react";
 import Navbar from "./Navbar";
 import "./globals.css";
+import { Head } from "next/head";
 
 export default function RootLayout({
   children,
@@ -9,10 +10,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" style={{ overflowX: "hidden" }}>
+      <Head>
+        <meta name="google-site-verification" content="na_tZta5zaB_Xxa0FQIp6p8Ul908Y2rjpRNgysQ_OBc" />
+        <title>MQuery, Meqr, Ayan Mahajan full stack developer portfolio website</title>
+        <meta name="description" content="MQuery, Meqr, Ayan Mahajan who is the full stack developer behind the app NuraNote's portfolio website" />
+        <meta name="keywords" content="MQuery, Meqr, Ayan Mahajan, full stack developer, portfolio, who is Ayan Mahjahan?, who is Meqr?, who is MQuery?" />
+        <meta name="author" content="Ayan Mahajan" />
+      </Head>
       <body style={{ overflowX: "hidden" }}>
         <NextUIProvider>
-        <Navbar />
-        {children}
+          <Navbar />
+          {children}
         </NextUIProvider>
       </body>
     </html>
